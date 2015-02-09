8d24d5e5aaf15f6903bde88bba69582add505172
Josh Johnson <jjohnson@adzerk.com>
2015-02-09 15:33:25 -0500

    Implemented search-based alternatives, updated docs

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Comparison between underscore's [intersect method](http://underscorejs.org/docs/
 
 We've also added a variant of the naive implementation that short circuits on the first match - this is not how underscore's intersection method works, but it fits our use case in bifrost more completely.
 
+In addition, we've implemented a search-based variant using regular expressions and simple sub-string matching.
+
 To run, 
 
 ```
@@ -13,10 +15,12 @@ Example output:
 
 ```
 $ node test.js
-dumb intersection x 1,034,802 ops/sec ±1.18% (93 runs sampled)
-underscore intersection x 236,263 ops/sec ±1.31% (91 runs sampled)
-dumb intersection - short circuit x 1,262,959 ops/sec ±2.25% (89 runs sampled)
-Fastest is dumb intersection - short circuit
+dumb intersection                 x   263,174 ops/sec ±1.69% (92 runs sampled)
+underscore intersection           x   114,146 ops/sec ±1.34% (92 runs sampled)
+dumb intersection - short circuit x   319,810 ops/sec ±1.20% (93 runs sampled)
+regex intersection                x 1,188,875 ops/sec ±1.83% (92 runs sampled)
+substring intersection            x 2,677,300 ops/sec ±1.82% (87 runs sampled)
+Fastest is substring intersection
 ```
 
 The test data is randomly generated, with a few guarantees so at least one match will occur.
--- test.js
+++ test.js
@@ -4,11 +4,37 @@ var chance = require('chance').Chance();
 
 var ad_keywords = ['foo', 'bar', 'baz', 'fred', 'barney', 'wilma'];
 var request_keywords = ['foo', 'baz', 'betty'];
-ad_keywords.concat(chance.unique(chance.word, 50));
-request_keywords.concat(chance.unique(chance.word, 20));
+ad_keywords = ad_keywords.concat(chance.unique(chance.word, 10));
+request_keywords = request_keywords.concat(chance.unique(chance.word, 3));
 
 ad_keywords = chance.shuffle(ad_keywords);
 request_keywords = chance.shuffle(request_keywords);
+
+ad_keywords = _.map(ad_keywords, function(x){return x.toLowerCase()});
+request_keywords = _.map(request_keywords, function(x){return x.toLowerCase()});
+
+
+
+var spacer = "\n";
+
+var regex_text = function(x, y){
+    var lines = x.join(spacer);
+    var regex = new RegExp("^("+y.join("|")+")$", "mi");
+    
+    return regex.test(lines);
+};
+
+var substr_text = function(x, y){
+    var lines = spacer+x.join(spacer)+spacer;
+    for(var i=0; i<y.length; i++){
+        var current = y[i];
+        if(lines.indexOf(spacer+current+spacer) >= 0){
+            return true;
+        }
+    }
+    
+    return false
+};
 
 var dumb = function(x, y){
     var holder = {};
@@ -45,7 +71,6 @@ var dumb_smarter = function(x, y){
     return false;
 };
 
-
 var suite = new benchmark.Benchmark.Suite;
 
 suite.add('dumb intersection', function() { 
@@ -56,6 +81,12 @@ suite.add('dumb intersection', function() {
 })
 .add('dumb intersection - short circuit',  function() { 
     dumb_smarter(ad_keywords, request_keywords);
+})
+.add('regex intersection',  function() { 
+    regex_text(ad_keywords, request_keywords);
+})
+.add('substring intersection',  function() { 
+    substr_text(ad_keywords, request_keywords);
 })
 .on('cycle', function(event) {
     console.log(String(event.target));
